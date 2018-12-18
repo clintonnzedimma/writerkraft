ec4620b481709ff7e2129607c6586bfa84ddf183
Clinton Nzedimma <34009341+clintonnzedimma@users.noreply.github.com>
2018-12-18 22:48:46 +0100

    Add files via upload

--- Engine/Controllers/content_ctrl.php
+++ Engine/Controllers/content_ctrl.php
@@ -1,7 +1,27 @@
 <?php
-User_Factory::protectPage();
 if (isset($_GET['id']) && Kraft_Factory::existsById($_GET['id'])) {
 	$kraft = new Kraft_Singleton($_GET['id']);
+	//Comments data
+	$comments_assoc = $kraft->allComments("ASC");
+	$comments = $comments_assoc['data'];
+
+	//adding comments 
+	if (isset($_POST['add-comment'])) {
+		$user_comment = sanitize_note($_POST['comment']);
+		// errors
+		if(empty($user_comment)) {
+			$errors[] = "Your comment should not be empty !";
+		}
+
+		if (!empty($errors)) {
+			print_r($errors);
+		} else {
+			$kraft->addComment($user_comment, $u->get('id'));
+			header("Location:content.php?id=".$kraft->get('id'));
+			exit();
+
+		}
+	}
 }
 
 ?>
