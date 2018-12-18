2ced70bb2a6ecf09b8244ef57d647b417034a60c
Clinton Nzedimma <34009341+clintonnzedimma@users.noreply.github.com>
2018-12-18 22:47:13 +0100

    Add files via upload

--- content.php
+++ content.php
@@ -21,11 +21,36 @@ if (isset($_GET['id']) && Kraft_Factory::existsById($_GET['id'])) :
 	<?=$kraft->get('writeup') ?>
 	<br>
 	<br>
-	Time : <?=$kraft->get('time_of_post') ?>. Date <?=$kraft->get('date_of_post') ?>
+	Time : <?=$kraft->get('time_of_post') ?>, Date <?=$kraft->get('date_of_post') ?>
 
 	<br>
 	Posted By <?=$kraft->creator->get('full_name')?>
 </p>
+
+
+<?php if (User_Factory::isLoggedIn()): ?>
+<p>
+	<h3>Comments</h3>
+	<form method="POST">
+		<textarea name="comment"></textarea>
+		<button name="add-comment">comment</button>	
+	</form>
+</p>	
+<?php endif ?>
+
+
+<div>
+<?php if ($comments): ?>
+	<?php foreach ($comments as $comment): ?>
+		<p style="border-bottom: #ccc 1px solid">
+			<?=$comment['comment']?>
+			<br>
+			<small><?=User_Factory::getById('full_name', $comment['user_id'])?> </small>
+		</p>
+	<?php endforeach ?>		
+<?php endif ?>		
+
+</div>
 
 <?php
 endif;
